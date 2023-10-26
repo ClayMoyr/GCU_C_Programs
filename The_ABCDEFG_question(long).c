9b4bf488915a6a980593e35798d59b3a18fb3358
ClayMoyr <149105455+ClayMoyr@users.noreply.github.com>
2023-10-27 00:02:43 +0530

    Create The_ABCDEFG_question(long).c
    The question given to us for homework on 26 October 2023

---
+++ The_ABCDEFG_question(long).c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main() {
+    double a, b, c, d, e, f, g;
+
+    // Input values for variables
+    printf("Enter the value for 'a': ");
+    scanf("%lf", &a);
+
+    printf("Enter the value for 'b': ");
+    scanf("%lf", &b);
+
+    printf("Enter the value for 'c': ");
+    scanf("%lf", &c);
+
+    printf("Enter the value for 'd': ");
+    scanf("%lf", &d);
+
+    printf("Enter the value for 'e': ");
+    scanf("%lf", &e);
+
+    printf("Enter the value for 'f': ");
+    scanf("%lf", &f);
+
+    printf("Enter the value for 'g': ");
+    scanf("%lf", &g);
+
+    // Evaluate the expression
+    double result = ((a + (b / (c * d) + e)) * (f + g));
+
+    // Display the result
+    printf("Result of the expression ((a+b/c*d+e)*(f+g)) is: %lf\n", result);
+
+    return 0;
+}
